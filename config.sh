93e7314f17b65b6bb555b5873bc3b4d5fb4ffb5f
d00m4n <github@doomans.com>
2021-08-13 06:03:04 +0200

    :All files created

---
+++ config.sh
@@ -0,0 +1,4 @@
+# create link for init.vim
+ln -f init.vim ~/.config/nvim/init.vim
+# create link for vimrc
+ls -s vimrc  ~/.vimrc
--- init.vim
+++ init.vim
@@ -1,1 +1,4 @@
-set number
+set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath=&runtimepath
+source ~/.vimrc
+
