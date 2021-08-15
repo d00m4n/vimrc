6f300eea54d577e966e03e7feccb191b9cab3ee8
d00m4n <github@doomans.com>
2021-08-16 00:13:46 +0200

    Update config.sh
    Fixed ln error

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 # create link for init.vim
 ln -f init.vim ~/.config/nvim/init.vim
 # create link for vimrc
-ls -s vimrc  ~/.vimrc
+ln -f vim.rc  ~/.vimrc
 # download plugin manager
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
