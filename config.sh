8a6c9a5255d5180b854e91bac555fe02dd2773c4
d00m4n <github@doomans.com>
2021-08-16 07:56:57 +0200

    Update config.sh
    added SUDO to create symbolic links

--- config.sh
+++ config.sh
@@ -1,9 +1,9 @@
 # create config-vim folder if no exists
 mkdir -p ~/.config/nvim
 # create link for init.vim
-ln -f init.vim ~/.config/nvim/init.vim
+sudo ln -f init.vim ~/.config/nvim/init.vim
 # create link for vimrc
-ln -f vim.rc  ~/.vimrc
+sudo ln -f vim.rc  ~/.vimrc
 # download plugin manager
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
