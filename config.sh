868c4be5803e150694ddfae7f35962194cb2cda2
d00m4n <github@doomans.com>
2021-08-16 08:07:28 +0200

    Update config.sh
    # Auto install plugins and themes.

--- config.sh
+++ config.sh
@@ -7,3 +7,7 @@ sudo ln -f vim.rc  ~/.vimrc
 # download plugin manager
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Auto install plugins and themes.
+nvim +PlugInstall +qall
+
